ae7dcd77aff12b9f974d6a1bd0d5ceb4dfee1ef3
Andrzej Kurtycz <andrzejkurtycz@gmail.com>
2023-05-30 12:40:47 +0200

    html, scss, js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,6 @@ const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
 const showError = (input, msg) => {
-	//argument input przechowuje nasze inputy
-	//argument msg przechowuje placeholder
-
 	const formBox = input.parentElement
 	const errorMsg = formBox.querySelector('.error-text')
 
@@ -22,7 +19,7 @@ const clearError = input => {
 	formBox.classList.remove('error')
 }
 
-const checkform = input => {
+const checkForm = input => {
 	input.forEach(el => {
 		if (el.value === '') {
 			showError(el, el.placeholder)
@@ -32,19 +29,66 @@ const checkform = input => {
 	})
 }
 
-//argument INPUT z funkcji "checkForm" przechowuje tablicę z naszymi inputami
-//argument EL odnosi się do każdej zmiennej, którą umieściliśmny w tablicy
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${input.previousElementSibling.innerText.slice(
+				0,
+				-1
+			)} skĹ‚ada siÄ™ z min. ${min} znakĂłw.`
+		)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'HasĹ‚a do siebie nie pasujÄ….')
+	}
+}
+
+const checkEmail = email => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest niepoprawny')
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
+}
 
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
 
-	checkform([username, pass, pass2, email])
+	checkForm([username, pass, pass2, email])
+	checkLength(username, 3)
+	checkLength(pass, 8)
+	checkPassword(pass, pass2)
+	checkEmail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
-	e.preventDefault()[(username, pass, pass2, email)].forEach(el => {
+	e.preventDefault()
+
+	;[username, pass, pass2, email].forEach(el => {
 		el.value = ''
+		clearError(el)
 	})
-	//odwołuje się do każdej zmiennej wartości value i przypisuje jej pustą wartość
-	//prevenDefault uniemozliwia stronie przeładowywanie za każdym razem jak naciśniemy button
 })
